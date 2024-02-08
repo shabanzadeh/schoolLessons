844286b1bf31d294f962d3a78187ef6d37ff7b9b
maryam <shabanzadeh1712@yahoo.com>
2024-02-08 12:46:04 +0100

    definde deleted function

--- src/App.js
+++ src/App.js
@@ -1,37 +1,63 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Course from "./Course";
 
 function App() {
-  const[courseList, setCourseList] = useState([]);
-  const [newCours, setCourse] = useState("");
+  const [courseList , setCourseList] = useState([]);
+  const [newCourse, setNewCourse] = useState("")
+ 
+  const handelChange =(event)=>{
+    setNewCourse(event.target.value)
 
-  const addCourse=()=>{
-    const newCourseList = [...courseList, newCours]
-    setCourseList(newCourseList)
+  }
+
+  const addCourse =() =>{
+    // let arr=[];
+    // const name ="sarvin"
+    // arr.push(name)
+    
+    const course = {
+      id : courseList.length === 0 ? 1 : courseList[courseList.length -1 ].id + 1 ,
+      courseName : newCourse,
+      isCompleted : false
+    }
+    setCourseList( [...courseList , course])
     console.log(courseList)
-  }
-
-  const handelChange=(event)=>{
-    return setCourse(event.target.value)
 
   }
+  const deleteCourse =(courseId) =>{
+    setCourseList(courseList.filter((course)=> courseId !== course.id ))
+  }
+
+  const completeCourse = (courseId)=>{
+    const newCourseList = courseList.map((course)=>{
+      if (course.id === courseId)
+       return {...course , isCompleted : !course.isCompleted}
+      else 
+        return course
+    })
+
+    setCourseList(newCourseList)
+  }
+
   return (
     <div className="App">
-      <div className='add-course'>
-        <input type='text'onChange={handelChange}></input>
-        <button onClick={addCourse}>Add course</button>
+      <div className="add-course">
+        <input type="text" onChange={handelChange}></input>
+        <button onClick={addCourse}>Add Course</button>
       </div>
-      <div className='list'></div>
-      {courseList.map((course)=>{
-        return (<div>
-          <h1>{course}</h1> 
-          <button>X</button>
-
-      </div>)
-    })}
-      
+      <div className="list">
+        { courseList.map ((course , index) =>{
+          return (
+            <Course key={index} course={course} 
+            deleteCourse={deleteCourse} 
+            completeCourse={completeCourse}
+            />
+          )
+        })}
+  
+      </div>
     </div>
   );
 }
-
 export default App;
